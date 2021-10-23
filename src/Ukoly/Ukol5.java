1f3428d8d40714ad32415f41337603421cbacbfd
Jan Sotona <sotona.honza@email.cz>
2021-10-23 15:36:26 +0200

    Ukol 5

---
+++ src/Ukoly/Ukol5.java
@@ -0,0 +1,69 @@
+package Ukoly;
+
+public class Ukol5 {
+    public static int max(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] array) {
+
+        int index = 0;
+        int min = array[index];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int sum(int[] array) {
+
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static int averageNum(int[] array) {
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        return total/ array.length;
+    }
+
+
+        public static void main (String[]args){
+            // Vytvorte 1D pole1 o velikosti 10 s čísly 1-10                                                   // DONE
+            // Vytvorte 1D pole2 o velikosti 10 s náhodnými čísli 0-10 // nevím zda jsem to pochopil správně   // DONE
+            int[] pole1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] pole2 = {5,8 ,17 ,9 ,20 ,30 ,25 ,15 ,18 ,35 };
+
+
+            // Vytvořte funkce zjistujici minimální hodnotu prvku v poli,                                      // DONE
+            // sumu a průměru prvků                                                                            //DONE
+            // Vypočítané hodnoty vypiště do konzole                                                           //DONE
+            System.out.println("Maximum number for pole1: "  + max(pole1));
+            System.out.println("Maximum number for pole2: "  + max(pole2));
+            System.out.println("Minimum number for pole1: "  + min(pole1));
+            System.out.println("Minimum number for pole2: "  + min(pole2));
+            System.out.println("Sum number for pole1 : "     + sum(pole1));
+            System.out.println("Sum number for pole1 : "     + sum(pole2));
+            System.out.println("Average number for pole1 : " + averageNum(pole1));
+            System.out.println("Average number for pole1 : " + averageNum(pole2));
+
+
+        }
+    }
+
+
+
---
+++ src/Ukoly/Ukol5_2.java
@@ -0,0 +1,41 @@
+package Ukoly;
+
+public class Ukol5_2 {
+        // TODO vytvorte pole o velikosti 10 s nahodnymi cisly 0-10
+        // vytvorte metodu setrid, ktera seradi prvky pole vzestupne
+        // napr: {5, 6, 7 ,3 } -> {3, 5, 6, 7}
+        // muzete vyuzit napr algoritmu bubble sort
+        static class BubbleSort
+        {
+            void bubbleSort(int array[])
+            {
+                int n = array.length;
+                for (int i = 0; i < n-1; i++)
+                    for (int j = 0; j < n-i-1; j++)
+                        if (array[j] > array[j+1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j+1];
+                            array[j+1] = temp;
+                        }
+            }
+
+            void printArray(int array[])
+            {
+                int n = array.length;
+                for (int i = 0; i < n; ++i)
+                    System.out.print(array[i] + " ");
+                System.out.println();
+            }
+
+
+            public static void main(String args[])
+            {
+                BubbleSort ob = new BubbleSort();
+                int array[] = {5,8 ,17 ,9 ,20 ,30 ,25 ,15 ,18 ,35 };
+                ob.bubbleSort(array);
+                System.out.println("Sorted numbers : ");
+                ob.printArray(array);
+            }
+        }
+}
